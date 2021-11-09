ade69aa4716c7fa0be5a90bb2aeaea9c52e1c062
尼古拉斯丶少 <sean.snow@live.com>
2021-11-09 18:05:56 +0800

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# @aomi/react-native-components-camera
+
+React Native Camera
+
+## Installation
+
+```sh
+npm install @aomi/react-native-components-camera
+```
+
+## Usage
+
+```js
+import { multiply } from "@aomi/react-native-components-camera";
+
+// ...
+
+const result = await multiply(3, 7);
+```
+
+## Contributing
+
+See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
+
+## License
+
+MIT
---
+++ android/src/main/java/com/aomireactnativecomponentscamera/ReactNativeComponentsCameraModule.java
@@ -0,0 +1,34 @@
+package com.aomireactnativecomponentscamera;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = ReactNativeComponentsCameraModule.NAME)
+public class ReactNativeComponentsCameraModule extends ReactContextBaseJavaModule {
+    public static final String NAME = "ReactNativeComponentsCamera";
+
+    public ReactNativeComponentsCameraModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    @NonNull
+    public String getName() {
+        return NAME;
+    }
+
+
+    // Example method
+    // See https://reactnative.dev/docs/native-modules-android
+    @ReactMethod
+    public void multiply(int a, int b, Promise promise) {
+        promise.resolve(a * b);
+    }
+
+    public static native int nativeMultiply(int a, int b);
+}
